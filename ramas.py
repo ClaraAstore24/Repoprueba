1493d599930b533a9125e341ed8250c79f56bd61
Clara Astore <claraastore13@gmail.com>
2022-11-06 07:15:06 -0300

    ramas

---
+++ ramas.py
@@ -0,0 +1,11 @@
+#CREAMOS MODULO
+
+class Persona:
+
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
+        
